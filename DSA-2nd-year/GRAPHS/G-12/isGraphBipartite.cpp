eb7d8f5dc463f5bd5da099f936325853c238a5ba
Saurav chaurasiya <127616848+HelloSaurav2305@users.noreply.github.com>
2024-02-27 13:52:50 +0530

    C:\Users\soura\OneDrive\Desktop\Programming\DSA-2nd-year\GRAPHS\G-12\isGraphBipartite.cpp

---
+++ DSA-2nd-year/GRAPHS/G-12/isGraphBipartite.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define ll long long
+typedef pair<int,int> pp;
+class Solution {
+public:
+
+    bool bfs(vector<vector<int>>& graph,vector<bool>&visited,int src,vector<int>&color){
+        queue<int>q;
+        q.push(src);
+        color[src]=1;
+        while(q.size()>0){
+            int _node=q.front();
+            cout<<_node<<" ";
+
+            q.pop();
+
+            if(visited[_node]) continue;
+
+            visited[_node]=true;
+            for(auto neighbour:graph[_node]){
+                if(visited[neighbour]) continue;
+
+                if(color[_node]==color[neighbour]) return false;
+                else{
+                    color[neighbour]=(color[_node]==0)?1:0;
+                    q.push(neighbour);
+                }
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        vector<bool>visited(graph.size(),-1);
+        vector<int>color(graph.size(),-1);
+        for(int i=0;i<graph.size();i++){
+            if(!visited[i]){
+                if(bfs(graph,visited,i,color)==false){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+int main() {
+    Solution s;
+    vector<vector<int>>graph={{1,3},{0,2},{1,3},{0,2}};
+    cout<<s.isBipartite(graph);
+    
+    return 0;
+}
